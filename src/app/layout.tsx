0d278dc193f87ce201e89a3ab8467b90b72c3287
BananexTT <bananareidelass@gmail.com>
2026-03-11 19:39:02 -0300

    atualização de erros

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import { Provider } from "../components/ui/provider";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body>
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
+}
